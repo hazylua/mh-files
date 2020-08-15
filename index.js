7a710d811516d3878d751ef1a3985e7744dca801
yb00 <40061395+yb00@users.noreply.github.com>
2020-08-15 14:21:55 -0300

    Add express configuration

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+app = express()
+
+app.get('/assets/armormap.json', (req, res) => {
+    res.sendFile(path.join(__dirname, '/assets/', 'armormap.json'));
+});
+
+app.get('/assets/charmmap.json', (req, res) => {
+    res.sendFile(path.join(__dirname, '/assets/', 'charmmap.json'));
+});
+
+app.get('/assets/decomap.json', (req, res) => {
+    res.sendFile(path.join(__dirname, '/assets/', 'decomap.json'));
+});
+
+app.get('/assets/skillmap.json', (req, res) => {
+    res.sendFile(path.join(__dirname, '/assets/', 'skillmap.json'));
+});
+
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}.`))
